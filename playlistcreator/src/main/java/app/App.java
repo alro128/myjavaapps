3864c85ea73ee910a4c2f45f94024f41e8b2afd6
alro128 <alvarovigil@alvarovigils-iMac.local>
2015-03-10 12:03:53 +0100

    create m3u playlist file

--- playlistcreator/src/main/java/app/App.java
+++ playlistcreator/src/main/java/app/App.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 
 import com.mpatric.mp3agic.InvalidDataException;
@@ -11,17 +12,16 @@ import com.mpatric.mp3agic.Mp3File;
 import com.mpatric.mp3agic.UnsupportedTagException;
 
 public class App {
-	
-	final static Logger log = Logger.getLogger(App.class);
-	
-	public static void main(String[] args) throws IOException {
-		
 
-		
+	final static Logger log = Logger.getLogger(App.class);
+
+	public static void main(String[] args) throws IOException {
+
 		File dir = new File(args[0]);
 
 		MyDirectoryWalker walker = new MyDirectoryWalker();
 		List<File> files = walker.getMyFiles(dir);
+		String line = "";
 
 		log.debug("\nFiles ending with .txt with a depth of 2 or less");
 		for (File file : files) {
@@ -29,7 +29,10 @@ public class App {
 			String playlist = file.getParentFile().getName();
 			try {
 				Mp3File mpt = new Mp3File(file.getCanonicalPath());
-				log.info("[" + playlist + "]" + mpt.getId3v2Tag().getArtist() + " - "+ mpt.getId3v2Tag().getTitle());
+				line = "[" + playlist + "]" + mpt.getId3v2Tag().getArtist() + " - " + mpt.getId3v2Tag().getTitle() + "\n";
+				log.info(line);
+				FileUtils.writeStringToFile(new File(playlist + ".m3u"), line, "UTF-8", true);
+
 			} catch (UnsupportedTagException e) {
 				log.error(e.getMessage());
 			} catch (InvalidDataException e) {
